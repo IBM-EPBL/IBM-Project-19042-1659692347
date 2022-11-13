6abe8c0b6d83654baeac9482fc834f2408b18f34
Nithish-Evoltech <nithish@evoltechgroup.com>
2022-11-13 19:47:52 +0530

    convert app to run by flask

---
+++ project/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html');
+
+@app.route("/wallet")
+def wallet():
+    return render_template('wallet.html');
+
+@app.route("/expenditure")
+def expenditure():
+    return render_template('expenditure.html');
+
+@app.route("/error")
+def error():
+    return render_template('error.html');
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True,port=4000)
